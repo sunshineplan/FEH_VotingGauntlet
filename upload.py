c2bb8e5014de114e46d5460467146bc844f3ffb8
sunshineplan <sunshineplan@users.noreply.github.com>
2020-04-08 22:49:35 +0800

    Upload 結果一覧

--- upload.py
+++ upload.py
@@ -26,7 +26,7 @@ PATH = config.get('github', 'PATH')
 PROXY = config.get('github', 'PROXY')
 
 
-def query(filter=None, projection=None, sort=None, limit=0):
+def query(filter_or_pipeline=None, projection=None, sort=None, limit=0, mode='find'):
     if MONGO_AUTH:
         username = quote_plus(MONGO_AUTH)
         password = quote_plus(MONGO_PASSWORD)
@@ -34,7 +34,10 @@ def query(filter=None, projection=None, sort=None, limit=0):
     else:
         URI = f'mongodb://{MONGO_SERVER}:{MONGO_PORT}'
     with MongoClient(URI) as client:
-        return list(client[MONGO_DATABASE][MONGO_COLLECTION].find(filter, projection, sort=sort, limit=limit))
+        if mode == 'find':
+            return list(client[MONGO_DATABASE][MONGO_COLLECTION].find(filter_or_pipeline, projection, sort=sort, limit=limit))
+        else:
+            return list(client[MONGO_DATABASE][MONGO_COLLECTION].aggregate(filter_or_pipeline))
 
 
 def commit(name, content):
@@ -52,7 +55,10 @@ def converter(jsonlist):
     output = '['
     i = 1
     for json in jsonlist:
-        json['date'] = str(json['date'].date())
+        try:
+            json['date'] = str(json['date'].date())
+        except KeyError:
+            pass
         output += dumps(json, ensure_ascii=False)
         if i < len(jsonlist):
             output += ',\n'
@@ -76,6 +82,25 @@ if __name__ == '__main__':
     data = query({'event': event}, {'_id': 0, 'event': 0},
                  [('round', 1), ('scoreboard', 1), ('date', 1), ('hour', 1)])
     respone = commit(name, converter(data))
-    if respone.status_code != 201:
-        print('Commit failed!\n')
+    if respone.status_code == 201:
+        print(f'{name}.json uploaded.')
+    else:
+        print(f'Upload {name}.json failed!\n')
+        print(respone.json()['message'])
+    name = f'FEH 投票大戦第{event}回結果一覧'
+    pipeline = []
+    pipeline.append({'$addFields': {'tmp': '$scoreboard'}})
+    pipeline.append({'$unwind': '$tmp'})
+    pipeline.append({'$group': {'_id': {'r': '$round', 'h': '$tmp.hero'},
+                                's': {'$max': '$scoreboard'}}})
+    pipeline.append({'$group': {'_id': {'r': '$_id.r', 's': '$s'}}})
+    pipeline.append(
+        {'$project': {'_id': 0, 'round': '$_id.r', 'scoreboard': '$_id.s'}})
+    pipeline.append({'$sort': {'round': 1, 'scoreboard': 1}})
+    data = query(pipeline, mode='aggregate')
+    respone = commit(name, converter(data))
+    if respone.status_code == 201:
+        print(f'{name}.json uploaded.')
+    else:
+        print(f'Upload {name}.json failed!\n')
         print(respone.json()['message'])
