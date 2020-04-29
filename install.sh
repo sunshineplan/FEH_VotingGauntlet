c40170cf5188f1baa886a81778d7684e7bee5468
sunshineplan <sunshineplan@users.noreply.github.com>
2020-04-29 10:25:03 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+installSoftware() {
+    apt -qq -y install python3-pymongo python3-requests python3-bs4
+}
+
+installFEH() {
+    curl -Lo- https://github.com/sunshineplan/FEH_VotingGauntlet/archive/v1.0.tar.gz | tar zxC /etc
+    mv /etc/FEH_VotingGauntlet* /etc/feh
+    chmod +x /etc/feh/*.py
+}
+
+createCronTask() {
+	cp -s /etc/feh/feh.cron /etc/cron.d
+}
+
+main() {
+    installSoftware
+    installFEH
+    createCronTask
+}
+
+main
